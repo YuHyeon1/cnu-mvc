117b6f227540a94265a3b6a51416ea54cbb2f94a
YuHyeon <cola4204@naver.com>
2025-04-17 09:33:27 +0900

    최종 수정

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -10,12 +10,20 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member){
+        if (memberRepository.findByEmail(member.getEmail())!=null){
+            throw new IllegalStateException("이미 존재하는 이메일 계정입니다.");
+        }
+
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
-        return findMember;
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null || !member.getPwd().equals(pwd)) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+        return member;
     }
 
     public Member findById(Long id) {
@@ -24,6 +32,7 @@ public class MemberService {
 
     // 구현
     public Member findByEmail(String email) {
+
         return memberRepository.findByEmail(email);
     }
 
